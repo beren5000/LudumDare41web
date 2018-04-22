68219bcc61e83425d81daf4e66a59f97db62bd9b
beren5000 <beren.monsalve@gmail.com>
2018-04-22 02:15:42 -0500

    no browseable

--- ludumdare41Web/settings.py
+++ ludumdare41Web/settings.py
@@ -105,6 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
